eed0cb7eb22e89e3f7809feee20c33310e1cea98
abhay-86 <abhaysinghpatel690@gmail.com>
2025-12-20 19:41:19 +0530

    Feature/Completed CSS chanllenge 19a nd 20

---
+++ components/css/Dec19.tsx
@@ -0,0 +1,18 @@
+export default function Dec19() {
+  return (
+    <div className="bg-[#7E6293] w-full h-full flex items-center justify-center">
+      <div className="bg-[#E8BA79] w-50 h-28 flex justify-end">
+        <div className="bg-[#7E6293] w-46 h-25 flex justify-end">
+            <div className="bg-[#E8BA79] w-46 h-19 mt-6">
+                <div className="bg-[#7E6293] w-36 h-15 mt-4 flex justify-start">
+                    <div className="bg-[#E8BA79] w-32 h-12 mt-3">
+                        <div className="bg-[#7E6293] w-28 h-8 mt-4 flex justify-start"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-[#E8BA79] w-6 h-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/css/Dec20.tsx
@@ -0,0 +1,14 @@
+export default function Dec20(){
+  return (
+    <>
+    <div className="bg-[#1C204E] w-full h-full flex items-center justify-center">
+        <div className="bg-[#dd6b4d] w-28 h-28  rounded-full flex items-center justify-center">
+            <div className="bg-[#FEF7F5] w-20 h-20 rounded-full flex col items-center justify-center gap-10">
+            <div className="bg-black w-2 h-6 "></div>
+            <div className="bg-black w-2 h-6 "></div>
+            </div>
+        </div>
+    </div>
+    </>
+  );
+};
