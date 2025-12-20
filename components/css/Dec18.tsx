b02535967ecaab7a0d995c76cff23e1b3aead2d0
abhay-86 <abhaysinghpatel690@gmail.com>
2025-12-21 02:22:46 +0530

    Feat/Done-challenge-Dec18-add-css

---
+++ components/css/Dec18.tsx
@@ -0,0 +1,12 @@
+export default function Dec18() {
+  return (
+    <div className="bg-[#F0CD48] w-full h-full flex items-center justify-center">
+        <div className="bg-[#1D7B47] grid grid-rows-3 w-48 h-28">
+            <div className="bg-[#08331B] flex w-16 h-9 justify-self-start"></div>
+            <div className="bg-[#08331B] flex w-16 h-9 justify-self-end"></div>
+            <div className="bg-[#08331B] flex w-16 h-9 justify-self-start"></div>
+        </div>
+
+    </div>
+  );
+}
