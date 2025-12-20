73c586204153b8ecfb9d67524dc3e2a6d733ad9e
abhay-86 <abhaysinghpatel690@gmail.com>
2025-12-21 01:23:42 +0530

    Feat/Upated-file-to-build-path-for-workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/css-challenges",
+  assetPrefix: "/css-challenges/",
 };
 
 export default nextConfig;
