3ca0fa377df674c3c02c12051d022726e49de997
abhay-86 <abhaysinghpatel690@gmail.com>
2025-12-21 13:24:52 +0530

    Feature/Added-new-section-css-challenge

---
+++ components/css/Dec15.tsx
@@ -0,0 +1,15 @@
+export default function Dec15() {
+    return (
+        <>
+        <div className="bg-[#0B2429] flex flex-col w-full h-full items-center justify-center">
+            <div>
+                <div className="bg-[#CE6F4A] w-19 h-5 rounded-t-lg self-start"></div>
+                <div className="bg-[#CE6F4A] w-30 h-20 flex rounded-b-lg rounded-tr-lg pt-4">
+                    <div className="bg-[#0B2429] w-30 h-3 pt-2"></div>
+                </div>
+            </div>
+            
+        </div>
+        </>
+    )
+}
